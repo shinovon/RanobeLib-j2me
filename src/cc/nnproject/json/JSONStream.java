bae85bf0b67263894b5f467cb568fe0d4499dfe7
Shinovon <arman200711@gmail.com>
2025-01-13 14:06:20 +0500

    Fix JSONStream

--- src/cc/nnproject/json/JSONStream.java
+++ src/cc/nnproject/json/JSONStream.java
@@ -489,6 +489,18 @@ public class JSONStream extends Reader {
 				sb.append(l = (char) Integer.parseInt(new String(chars), 16));
 				continue;
 			}
+			if (c == 'n' && l == '\\') {
+				sb.append('\n');
+				continue;
+			}
+			if (c == 'r' && l == '\\') {
+				sb.append('\r');
+				continue;
+			}
+			if (c == 't' && l == '\\') {
+				sb.append('\t');
+				continue;
+			}
 			if (c == 0 || (l != '\\' && c == '"')) break;
 			sb.append(c);
 			l = c;
@@ -633,8 +645,7 @@ public class JSONStream extends Reader {
         iBuf = null;
         iBufAmount = 0;
         iBufPos = 0;
-        if (reader != null)
-        {
+        if (reader != null) {
             reader.close();
         }
     }
@@ -645,8 +656,7 @@ public class JSONStream extends Reader {
     public int read() throws IOException
     {
         int result = 0;
-        if (iBufPos >= iBufAmount)
-        {
+        if (iBufPos >= iBufAmount) {
             result = fillBuf();
         }
         if (result > -1)
